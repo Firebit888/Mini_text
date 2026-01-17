96ca6cb240a0148509d6d3724f863306de4a6606
Firebit888 <joaobernardo888@gmail.com>
2026-01-17 15:06:06 +0000

    Atualização do projecto create_mini_txt após anos

--- create_mini_txt.py
+++ create_mini_txt.py
@@ -1,6 +1,4 @@
-from os import *
-from io import *
-from colored import *
+from colored import fg
 
 green = fg('green')
 magenta = fg('magenta')
@@ -8,45 +6,23 @@ red = fg('red')
 blue = fg('blue')
 white = fg('white')
 
+# Nome do ficheiro
+filename = input(green + "Write your file name: ") + ".txt"
 
-read = input(green + "write your file: ")
-r = ".txt"
-y = read + r
-write_s = input(magenta + "write your text: \n" + white +  "1º - ")
-write_t = input(white + "2º - ")
-write_u = input(white + "3º - ")
-write_p = input(white + "4º - ")
-write_q = input(white + "5º - ")
-write_f = input(white + "6º - ")
-write_v = input(white + "finish - ")
+print(magenta + "Write your text (type ':exit' to finish)\n")
 
+with open(filename, "w", encoding="utf-8") as file:
+    line_number = 1
 
+    while True:
+        text = input(white + f"{line_number}º - ")
 
-def save_key():
-    green = fg('green')
-    magenta = fg('magenta')
-    red = fg('red')
-    blue = fg('blue')
-    new_file = open(y, "w+")
-    new_file.write(write_s + '\n')
-    new_file.write(write_t + '\n')
-    new_file.write(write_u + '\n')
-    new_file.write(write_p + '\n')
-    new_file.write(write_q + '\n')
-    new_file.write(write_f + '\n')
-    new_file.write(write_v + '\n')
-    print(red + "Can´t write more! \n")
-    print(green + "Thanks for use this program. \n")
-    new_file.close()
+        if text.lower() == ":exit":
+            print(red + "\nCan't write more!")
+            break
 
-def keyread():
-    green = fg('green')
-    magenta = fg('magenta')
-    red = fg('red')
-    blue = fg('blue')
-    print(blue + "The name you file is " + read + ".txt")
+        file.write(text + "\n")
+        line_number += 1
 
-
-save_key()
-keyread()
-input(white + "Press a keyword to close")
+print(green + "Thanks for using this program.")
+input(white + "Press ENTER to close")
